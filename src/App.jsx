8e1b7e0b507675ff1c5856938fd346325e570ded
RenatoNunes22 <renatonunes0011@hotmail.com>
2022-09-11 21:43:30 -0300

    feat: add react-alert setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import Tasks from "./components/Tasks";
+import { transitions, positions, Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
 
+const options = {
+   // you can also just use 'bottom center'
+   position: positions.BOTTOM_CENTER,
+   timeout: 3000,
+   offset: "30px",
+   // you can also just use 'scale'
+   transition: transitions.SCALE,
+};
 export const App = () => {
    return (
       <>
-         <Tasks />
+         <AlertProvider template={AlertTemplate} {...options}>
+            <Tasks />
+         </AlertProvider>
       </>
    );
 };
--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
+import { FaPlus } from "react-icons/fa";
 import "./AddTask.scss";
+import CustomButtom from "./CustomButtom";
 import CustomInput from "./CustomInput";
 const AddTask = () => {
    const [task, setTask] = useState("");
@@ -9,6 +11,9 @@ const AddTask = () => {
    return (
       <div className="add-task-container">
          <CustomInput label="Add task..." value={task} onChange={onChange} />
+         <CustomButtom>
+            <FaPlus size={14} color="#ffffff"></FaPlus>
+         </CustomButtom>
       </div>
    );
 };
